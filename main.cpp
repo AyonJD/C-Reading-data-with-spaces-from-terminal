e353996aec3d5bdd9d4875b0d07e1d679dba6340
Ayon Jodder <83225232+AyonJD@users.noreply.github.com>
2023-01-22 20:27:38 +0600

    Input getting from terminal

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <string> //To allow this program to store string
 
 int main(){
-    auto result = (10 <=> 20) > 0;
-    std::cout << result << std::endl;
+
+    // Printing error message
+    std::cerr << "Something went wrong" << std::endl;
+
+    // Printing log message
+    std::clog << "This is the log message" << std::endl;
+
+    std::string first_name;
+    int age;
+
+    // Printing in sonsole (terminal)
+    std::cout << "Please enter your name and age seperate with space" << std::endl;
+
+    // Reading from terminal
+    std::cin >> first_name;
+    std::cin >> age;
+
+    std::cout << "Your name is " << first_name << " & age is " << age << std::endl;
+
+    return 0;
 }
